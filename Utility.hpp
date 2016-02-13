eea0287d70901055af4f6c41cd5167bc7d1e9aae
tapetums <tapetums@live.jp>
2016-02-14 00:08:27 +0900

    Initial Commit

---
+++ Utility.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+//---------------------------------------------------------------------------//
+//
+// Utility.hpp
+//  TTB Plugin Template (C++11)
+//
+//---------------------------------------------------------------------------//
+
+#include <windows.h>
+
+#include "Plugin.hpp"
+
+//---------------------------------------------------------------------------//
+
+LPTSTR       CopyString          (LPCTSTR Src);
+void         DeleteString        (LPCTSTR Str);
+PLUGIN_INFO* CopyPluginInfo      (const PLUGIN_INFO* Src);
+void         FreePluginInfo      (PLUGIN_INFO* PLUGIN_INFO);
+void         GetVersion          (LPTSTR Filename, DWORD* VersionMS, DWORD* VersionLS);
+void         WriteLog            (ERROR_LEVEL logLevel, LPCTSTR format, ...);
+BOOL         ExecutePluginCommand(LPCTSTR pluginName, INT32 CmdID);
+
+//---------------------------------------------------------------------------//
+
+// Utility.hpp
